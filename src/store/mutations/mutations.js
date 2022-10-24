7372ac3934b6b59ee9b08b7f00f40e9c78f93ce4
huasenjio <184820911@qq.com>
2022-10-25 00:14:07 +0800

    fix：修复vuex万能提交方法语法错误

--- src/store/mutations/mutations.js
+++ src/store/mutations/mutations.js
@@ -2,7 +2,7 @@
  * @Autor: huasenjio
  * @Date: 2021-08-25 01:53:35
  * @LastEditors: huasenjio
- * @LastEditTime: 2022-09-30 01:02:21
+ * @LastEditTime: 2022-10-25 00:13:15
  * @Description:
  */
 
@@ -20,7 +20,7 @@ export default {
           // 类型相等时，若state值不为对象，则直接覆盖
           if (Object.prototype.toString.call(s[key]) === '[object Object]') {
             // 更新值为对象，并且不为空，则需要递归遍历，否则直接覆盖
-            if (Object.prototype.toString.call(p[key]) === '[object Object]' && Object.keys(p[key].length !== 0)) {
+            if (Object.prototype.toString.call(p[key]) === '[object Object]' && Object.keys(p[key]).length !== 0) {
               merge(s[key], p[key]);
             } else {
               s[key] = p[key];
